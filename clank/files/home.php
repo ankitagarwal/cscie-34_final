62fc4cdbf620cffe7d7c64f6e957dae154d17ede
Ankit Agarwal <ankit.frnz@gmail.com>
2015-12-21 17:56:55 +0530

    More fixes

--- clank/files/home.php
+++ clank/files/home.php
@@ -74,7 +74,7 @@
                             </span>
                         </div>
                         <div class="row container-fluid">
-                            <span class="col-sm-offeset-6">Category:</span>
+                            <span class="col-sm-offset-6">Category:</span>
                             <span class="greyfont">{{get_category()}}</span>
                         </div>
                     </div>
